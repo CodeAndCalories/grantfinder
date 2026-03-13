1bb35ae48453bcc779d34243da516a51c5d060a5
Gambino <axigamingclips@gmail.com>
2026-03-13 00:26:34 -0400

    add adsense verification script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import SiteNav from "@/components/SiteNav";
 
@@ -14,6 +15,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7606786471448802"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
+
       <body>
         <header>
           <div className="header-inner">
@@ -21,6 +31,7 @@ export default function RootLayout({
             <SiteNav />
           </div>
         </header>
+
         <main>{children}</main>
       </body>
     </html>
